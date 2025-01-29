532f191a30f613525c5d1d53a0bc95af89819010
Wharkk <brieuc.guillermic@gmail.com>
2025-01-29 12:52:49 +0100

    Fix of some erorrs in the controller
    Fixed some errors in the controller (e.g: useless use statements).

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\DTO\ContactDTO;
-use App\EventSubscriber\ContactRequestEvent;
 use App\Form\ContactType;
 use App\Message\EmailMessage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -20,7 +18,7 @@ final class ContactController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request, EventDispatcherInterface $dispatcher): Response
+    public function contact(Request $request): Response
     {
         $data = new ContactDTO();
         $form = $this->createForm(ContactType::class, $data);
